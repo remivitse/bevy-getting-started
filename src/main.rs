ab80b68c01746d20897236649763e8c50875e719
Rémi Vitse <remivitse@gmail.com>
2026-05-18 01:14:34 +0200

    feat: add HelloPlugin to modularize systems

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 pub mod component;
 pub mod entity;
+pub mod plugin;
 pub mod system;
 
-use crate::system::add_people::add_people;
-use crate::system::greet_people::greet_people;
-use crate::system::hello_world::hello_world;
-use crate::system::update_people::update_people;
+use crate::plugin::hello::HelloPlugin;
 use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
         .run();
 }
