2a640381ed59f2bf36d7ddee0d5b7a30144a8b07
Rémi Vitse <remivitse@gmail.com>
2026-05-18 01:19:04 +0200

    feat: remove `hello_world` system

--- src/plugin/hello.rs
+++ src/plugin/hello.rs
@@ -1,6 +1,5 @@
 use crate::system::add_people::add_people;
 use crate::system::greet_people::greet_people;
-use crate::system::hello_world::hello_world;
 use crate::system::update_people::update_people;
 use bevy::prelude::IntoScheduleConfigs;
 use bevy::prelude::{App, Plugin, Startup, Update};
@@ -10,6 +9,6 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, add_people);
-        app.add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+        app.add_systems(Update, (update_people, greet_people).chain());
     }
 }
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,4 +1,3 @@
 pub mod add_people;
 pub mod greet_people;
-pub mod hello_world;
 pub mod update_people;
