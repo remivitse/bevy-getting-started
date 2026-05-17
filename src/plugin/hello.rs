38e5cb38676f238aa0e4010bd54d0c8aa8afa88a
remivitse <81271504+remivitse@users.noreply.github.com>
2026-05-18 01:16:10 +0200

    feat: use plugins (#5)
    * feat: add HelloPlugin to modularize systems
    
    * chore: bump version to 0.4.0 in Cargo.toml and Cargo.lock

---
+++ src/plugin/hello.rs
@@ -0,0 +1,15 @@
+use crate::system::add_people::add_people;
+use crate::system::greet_people::greet_people;
+use crate::system::hello_world::hello_world;
+use crate::system::update_people::update_people;
+use bevy::prelude::IntoScheduleConfigs;
+use bevy::prelude::{App, Plugin, Startup, Update};
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+    }
+}
