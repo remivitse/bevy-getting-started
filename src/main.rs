98cfc6f65284ce4a45a96c4c39e03915e83cb4f8
Rémi Vitse <remivitse@gmail.com>
2026-05-17 23:17:06 +0200

    feat: implement basic ECS setup with systems and components

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+pub mod component;
+pub mod entity;
+pub mod system;
+
+use crate::system::add_people::add_people;
+use crate::system::greet_people::greet_people;
+use crate::system::hello_world::hello_world;
+use crate::system::update_people::update_people;
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run();
 }
---
+++ src/system/add_people.rs
@@ -0,0 +1,9 @@
+use crate::component::name::Name;
+use crate::component::person::Person;
+use bevy::prelude::Commands;
+
+pub fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
